1be460c9fa1685617c82529fbe49a3eceb1c865d
Chris DeLashmutt <cdelashmutt@pivotal.io>
2019-04-25 13:15:56 -0400

    Update file

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM ruby:2.5-alpine
 ENV PATH "/usr/local/bundle/bin:${PATH}"
 
 RUN apk add --no-cache musl-dev gcc make g++ curl jq zlib zlib-dev ruby-dev openssl libxslt-dev readline-dev sqlite sqlite-dev git bash build-base file linux-headers libc6-compat
-RUN gem install cf-uaac -v 3.2.0 --no-rdoc --no-ri
+RUN gem install cf-uaac --no-document
 
-COPY deps/bosh /usr/local/bin/bosh
-COPY deps/om /usr/local/bin/om
-COPY deps/credhub /usr/local/bin/credhub
+COPY bosh-cli/bosh /usr/local/bin/bosh
+COPY om/om /usr/local/bin/om
+COPY credhub-cli/credhub /usr/local/bin/credhub
--- grab-deps.sh
+++ grab-deps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 function get_latest_release {
+    mkdir -p $3
     url=$(curl -s -L https://api.github.com/repos/$1/releases/latest | jq -r '.assets[] | select(.browser_download_url | contains("'$2'")) | .browser_download_url')
     base="$3/$(basename $url)"
-    wget -q -nv -O $base $url
+    wget -O $base $url
     if [ ! -f $base ]; then
       echo "Failed to download latest release of $1 for platform $2 to $base"
       exit 1
@@ -11,13 +12,10 @@ function get_latest_release {
     chmod +x $base
 }
 
-mkdir -p deps
-rm  deps/*
-
-get_latest_release cloudfoundry/bosh-cli linux-amd64 deps
-mv deps/bosh-cli-*-linux-amd64 deps/bosh && chmod +x deps/bosh
-get_latest_release pivotal-cf/om linux deps
-mv deps/om-linux deps/om && chmod +x deps/om
-get_latest_release cloudfoundry-incubator/credhub-cli linux deps
-tar -xzf deps/credhub-linux-*.tgz -C deps && chmod +x deps/credhub
-rm deps/credhub-linux-*.tgz
+get_latest_release cloudfoundry/bosh-cli linux-amd64 bosh-cli
+mv bosh-cli/bosh-cli-*-linux-amd64 bosh-cli/bosh && chmod +x bosh-cli/bosh
+get_latest_release pivotal-cf/om linux om
+mv om/om-linux om/om && chmod +x om/om
+get_latest_release cloudfoundry-incubator/credhub-cli linux credhub-cli
+tar -xzf credhub-cli/credhub-linux-*.tgz -C credhub-cli && chmod +x credhub-cli/credhub
+rm credhub-cli/credhub-linux-*.tgz
